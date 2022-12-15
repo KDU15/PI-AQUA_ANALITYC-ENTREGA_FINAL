70db7cba258869001daf6c3ee987d8634c3c1e86
IanTth <ianrendrick@gmail.com>
2022-12-15 18:54:44 -0300

    Maps do Leaflet funcionando

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+
+  map: L.Map;
 
   constructor(
     private router: Router
@@ -17,6 +19,17 @@ export class MapPage implements OnInit {
   };
 
   ngOnInit() {
+
+
+  }
+  ionViewDidEnter(){
+    this.map = L.map('map').setView([-8.086734, -34.904810], 13);
+    L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{subdomains:['mt0','mt1','mt2','mt3']
+    }).addTo(this.map)
+
+    const markPoint = L.marker([-8.086734, -34.904810]);
+    markPoint.bindPopup('<p>Ponto Teste</p>')
+    this.map.addLayer(markPoint);
   }
 
 }
